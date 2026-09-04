02a18ab4a1d7cb52bdc34b8014f6bedafc52e2b6
SadhanyaSaravanan <sadhanya2278@gmail.com>
2026-09-04 10:43:45 +0530

    Add files via upload
    1. **Increasing Triangle Pattern:** Prints numbers `1` through `i` where the row count `i` increases from 1 to 5.
    2. **Decreasing Triangle Pattern:** Prints numbers `1` through `i` where the row count `i` decreases from 5 down to 1.
    
    ### Output
    ```text
    1
    12
    123
    1234
    12345
    
    12345
    1234
    123
    12
    1

---
+++ triangle.java
@@ -0,0 +1,30 @@
+//increasing triangle
+public class triangle
+{
+	public static void main(String[] args) {
+	    for(int i=1;i<=5;i++){
+	        for(int j=1;j<=i;j++){
+	            System.out.print(j);
+	        }
+	        System.out.println();
+	    }
+	    System.out.println();
+	    
+	    
+// decreasing triangle
+          for(int i=5;i>=1;i--){
+	        for(int j=1;j<=i;j++){
+	            System.out.print(j);
+	        }
+	        System.out.println();
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+}
+}
